846725e88c75126df8601c998609698aef93be8c
Lucianosc <scaminaci.luciano@gmail.com>
2024-08-03 21:20:58 -0300

    minor fixes on Footer, Header & SwitchTheme

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -3,10 +3,17 @@
 import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { Bars3Icon, BugAntIcon, ScaleIcon, WrenchIcon } from "@heroicons/react/24/outline";
+import { HeaderMenuLinks } from "./HeaderMenuLinks";
+import {
+  ArchiveBoxArrowDownIcon,
+  ArchiveBoxIcon,
+  Bars3Icon,
+  BugAntIcon,
+  ScaleIcon,
+  WrenchIcon,
+} from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
-import { HeaderMenuLinks } from "./HeaderMenuLinks";
 
 type HeaderMenuLink = {
   label: string;
@@ -27,7 +34,7 @@ export const menuLinks: HeaderMenuLink[] = [
   {
     label: "Create Ballot",
     href: "create",
-    icon: <WrenchIcon className="w-4 h-4" />,
+    icon: <ArchiveBoxIcon className="w-4 h-4" />,
   },
   {
     label: "Join",
@@ -37,9 +44,8 @@ export const menuLinks: HeaderMenuLink[] = [
   {
     label: "Vote",
     href: "vote",
-    icon: <ScaleIcon className="w-4 h-4" />,
+    icon: <ArchiveBoxArrowDownIcon className="w-4 h-4" />,
   },
-
 ];
 
 /**
@@ -79,14 +85,9 @@ export const Header = () => {
           )}
         </div>
         <Link href="/" passHref className="items-center hidden gap-2 ml-4 mr-6 lg:flex shrink-0">
-          <div className="relative flex w-10 h-10">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-          </svg>
-</div>
+          {/* App icon */}
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">SuperSecret-Ballot</span>
-            <span className="text-xs">Solidity Bootcamp - Group 1#</span>
+            <span className="font-bold leading-tight">Enigma Vote</span>
           </div>
         </Link>
         <ul className="hidden gap-2 px-1 lg:flex lg:flex-nowrap menu menu-horizontal">
